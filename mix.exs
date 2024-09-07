0a0e4c676015ee2f0a31d6013af25ebc29983fae
boeseSchwingung <boeseSchwingung@gmx.de>
2024-09-07 22:44:00 +0200

    chore: update patch version to v0.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DbSeeds.MixProject do
   def project do
     [
       app: :db_seeds,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
